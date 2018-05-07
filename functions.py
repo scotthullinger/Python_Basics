5ebd036d55f01b9bc013e0e55e72a3a1582d11d3
Scott Hullinger <sbhullinger@gmail.com>
2018-05-07 11:59:41 -0700

    addtion of function

--- functions.py
+++ functions.py
@@ -9,3 +9,9 @@ def lumberjack(name, pronoun):
 lumberjack("Kenneth", "he's")
 lumberjack("Lacey", "she's")
 lumberjack("Sam", "they're")
+
+def average(num1, num2):
+    return (num1+num2) / 2
+
+avg = average(2, 8)
+print(avg)
